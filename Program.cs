d586094316d4400f7dadf5009f51a5a0713b5624
fujikawa <?=utfpr#01brasil=?>
2023-08-24 15:11:43 -0300

    Implementação da execução para visualização do uso do adaptador.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using fluentvalidation_adapter_example.Domain.Commands;
+using fluentvalidation_adapter_example.Domain.Handlers;
+using fluentvalidation_adapter_example.Infra.Validators;
+
+// Criação do Handler com injeção da dependência do validator
+var handler = new AddPersonHandler(new AddPersonCommandValidator());
+
+// Comando inválido
+Console.WriteLine("Execução do primeiro comando....");
+
+var invalidCommand = new AddPersonCommand("Ab", 17);
+await handler.ExecuteAsync(invalidCommand);
+
+Console.WriteLine("-------------------------------------");
+
+// Comando válido
+Console.WriteLine("Execução do segundo comando....");
+
+var validCommand = new AddPersonCommand("José", 18);
+await handler.ExecuteAsync(validCommand);
