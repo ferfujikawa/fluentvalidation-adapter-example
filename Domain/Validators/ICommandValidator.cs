c1c00e19ba468d4854d6b00b27bebcd75359b8a9
fujikawa <?=utfpr#01brasil=?>
2023-08-24 15:09:37 -0300

    Implementação do adaptador de FluentValidation.

---
+++ Domain/Validators/ICommandValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace fluentvalidation_adapter_example.Domain.Validators
+{
+    public interface ICommandValidator<T>
+    {
+        public bool IsValid { get; }
+        public IEnumerable<string> Errors { get; }
+
+        public abstract Task ValidateExecAsync(T objectToValidate);
+    }
+}
---
+++ Infra/Validators/AbstractCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using fluentvalidation_adapter_example.Domain.Validators;
+
+namespace fluentvalidation_adapter_example.Infra.Validators
+{
+    public abstract class AbstractCommandValidator<T> : AbstractValidator<T>, ICommandValidator<T>
+    {
+        protected ValidationResult _result { get; set; } = new ValidationResult();
+        public bool IsValid => _result.IsValid;
+        public IEnumerable<string> Errors => _result.Errors.Select(x => x.ErrorMessage);
+
+        protected AbstractCommandValidator()
+        {
+            LoadRules();
+        }
+
+        public async Task ValidateExecAsync(T command) => _result = await ValidateAsync(command);
+
+        protected abstract void LoadRules();
+    }
+}
