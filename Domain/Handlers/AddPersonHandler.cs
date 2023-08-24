3a4091b0a67fd722fbca4b33d0c9565da68a611b
fujikawa <?=utfpr#01brasil=?>
2023-08-24 15:11:10 -0300

    Criação do handler de adição de pessoa.

---
+++ Domain/Handlers/AddPersonHandler.cs
@@ -0,0 +1,28 @@
+﻿using fluentvalidation_adapter_example.Domain.Commands;
+using fluentvalidation_adapter_example.Domain.Validators;
+
+namespace fluentvalidation_adapter_example.Domain.Handlers
+{
+    public class AddPersonHandler
+    {
+        ICommandValidator<AddPersonCommand> _validator;
+
+        public AddPersonHandler(ICommandValidator<AddPersonCommand> validator)
+        {
+            _validator = validator;
+        }
+
+        public async Task ExecuteAsync(AddPersonCommand command)
+        {
+            await _validator.ValidateExecAsync(command);
+            if (!_validator.IsValid)
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, _validator.Errors));
+                Console.WriteLine($"{command.Name} de {command.Age} anos não adicionado(a)");
+                return;
+            }
+
+            Console.WriteLine($"{command.Name} de {command.Age} anos adicionado(a)");
+        }
+    }
+}
