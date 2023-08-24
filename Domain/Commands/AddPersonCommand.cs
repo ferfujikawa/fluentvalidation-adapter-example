4eb37d05dba37256aceca455c1f56001025586d1
fujikawa <?=utfpr#01brasil=?>
2023-08-24 15:10:42 -0300

    Criação de comando para adição de pessoa. Implementação de validador para o comando de adição de pessoa.

---
+++ Domain/Commands/AddPersonCommand.cs
@@ -0,0 +1,16 @@
+﻿namespace fluentvalidation_adapter_example.Domain.Commands
+{
+    public class AddPersonCommand
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public int Age { get; set; }
+
+        public AddPersonCommand(string name, int age)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ Infra/Validators/AddPersonCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using fluentvalidation_adapter_example.Domain.Commands;
+
+namespace fluentvalidation_adapter_example.Infra.Validators
+{
+    public class AddPersonCommandValidator : AbstractCommandValidator<AddPersonCommand>
+    {
+        protected override void LoadRules()
+        {
+            RuleFor(x => x.Name).MinimumLength(3).WithMessage("O nome deve ter no mínimo 3 caracteres");
+            RuleFor(x => x.Age).GreaterThanOrEqualTo(18).WithMessage("Este sistema permite apenas o cadastro de pessoas com no mínimo 18 anos");
+        }
+    }
+}
